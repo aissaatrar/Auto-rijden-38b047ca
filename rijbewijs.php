0e5c5e8d35103741b2113370439d28b2161a779e
ilias atrar <23261@ma-web.nl>
2019-11-27 10:34:29 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$userAge;
+$minimumAge;
+echo "Hoe oud ben je? " . "\n";
+$userAge = readline();
+if ($userAge <= 16)
+{
+  echo "Helaas, je mag nog niet beginnen met rijlessen ";
+}
+else
+{
+  echo "Je mag beginnen met rijlessen!  ";
+}
+
+ ?>
